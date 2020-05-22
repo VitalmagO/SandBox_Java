3e5ca86127d21a3492788295d10cd153b010735f
Vital <vitalmago.job@gmail.com>
2020-05-22 17:57:11 +0600

    HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * A program that prints <code>Hello World!</code>
+ *
+ * @author Vital
+ * @version 1.0
+ */
+public class HelloWorld {
+    /**
+     * Program entry point.
+     *
+     * @param comandLineArgs command-line arguments
+     */
+    public static void main(String[] comandLineArgs) {
+        System.out.println("Hello World!");
+    }
+}
